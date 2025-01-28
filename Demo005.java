b18655eebb275bc464a5dd1061c9f121cdc7f181
raju03827 <rajuvadkapuram03@gmail.com>
2025-01-28 11:07:21 +0530

    username and password

---
+++ Demo005.java
@@ -0,0 +1,14 @@
+
+
+public class Demo005 {
+    public static void main(String[] args) {
+          String Username = "Ak";
+          String pwd = "Akshaya";
+          while((Username == "Ak") && (pwd =="Akshaya")) {
+            System.out.println("hello Ak Happy new year");
+            break;
+          }
+
+    }
+    
+}
