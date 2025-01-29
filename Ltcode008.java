61b8e64eb9aba74cb70db9829acaada185e1cc17
raju03827 <rajuvadkapuram03@gmail.com>
2025-01-29 11:39:16 +0530

    Merge Sorted Array

---
+++ Ltcode008.java
@@ -0,0 +1,40 @@
+public class Ltcode008 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for the last position in nums1
+
+        // While there are elements to compare in nums1 and nums2
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are remaining elements in nums2, copy them
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Ltcode008 Ltcode008 = new Ltcode008();
+        
+        // Example 1
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        Ltcode008.merge(nums1, m, nums2, n);
+        System.out.println(java.util.Arrays.toString(nums1)); // Output: [1, 2, 2, 3, 5, 6]
+
+       
+    }
+}
